225d05b244c4b9a56d431cb1a486c15a54a74055
12AbelCabreraMiranda <leg.abelr@gmail.com>
2020-02-29 20:58:46 -0600

    Wep APi con Xamarin Forms

---
+++ RESTListView/RESTListView.Android/obj/Debug/90/android/typemap.jm.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 6877587dd9df1ee0d24f49d10c864604cfcf3de2 */
+	.arch	armv8-a
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1343
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 351867
+	.include	"typemap.jm.inc"
---
+++ RESTListView/RESTListView/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RESTListView
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            LlenarCursos();
+        }
+        private async void LlenarCursos()
+        {
+            HttpClient cliente = new HttpClient();
+            string url = "http://tgconsulting.online/tg-rest/servicio.php/cursos";
+            var resultado = await cliente.GetAsync(url);
+            var json = resultado.Content.ReadAsStringAsync().Result;
+
+            CursosModel modelo = CursosModel.FromJson(json);
+            listaCursos.ItemsSource = modelo.Cursos;
+        }
+
+        private async void Agregar_Click(object sender, EventArgs args)
+        {
+            Curso curso = new Curso();
+            curso.Nombre = "Xamarin desde cero";
+            curso.Duracion = 30;
+            curso.Imagen = "https://xamarinhelp.com/wp-content/uploads/2017/06/TraditionalvsForms.png";
+
+            HttpClient cliente = new HttpClient();
+            string url = "http://tgconsulting.online/tg-rest/servicio.php/cursos";
+
+            String jsonCurso = JsonConvert.SerializeObject(curso);
+            var resultado = await cliente.PostAsync(url, new StringContent(jsonCurso));
+            var json = resultado.Content.ReadAsStringAsync().Result;
+
+            await DisplayAlert("Resultado", json, "Ok");
+
+            LlenarCursos();
+        }
+
+        private void Eliminar(object sender, EventArgs args)
+        {
+
+        }
+    }
+}
